1960aa095052720ef6f30394dba29d6f08ac234e
Luiz Gustavo Finotello <you@example.comfinotello22@hotmail.com>
2022-01-02 17:19:18 -0300

    Implementando JWT para login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientsController;
@@ -15,8 +16,17 @@ use App\Http\Controllers\ClientsController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::resource('clients', ClientsController::class, ['parameters' => ['clients' => 'id']]);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    Route::resource('clients', ClientsController::class, ['parameters' => ['clients' => 'id']]);
+});
